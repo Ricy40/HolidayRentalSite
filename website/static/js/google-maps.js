0ac72201587c901ad302ab70d3e6f207ae189c49
Ricy40 <ricardogarcia.pt@outlook.com>
2023-09-28 20:40:19 +0100

    Mostly finished minus pictures and french.

--- website/static/js/google-maps.js
+++ website/static/js/google-maps.js
@@ -1,58 +1,43 @@
 let map;
+let historical;
+let activities;
+let landscapes;
+let shops;
+
+var markersArray = [];
 
 async function initMap() {
     const { Map } = await google.maps.importLibrary("maps");
+    const { PinElement } = await google.maps.importLibrary("marker");
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+    historical = true;
+    activities = true;
+    landscapes = true;
+    shops = true;
+
     map = new Map(document.getElementById("map"), {
+        mapId: '3ce978e4be4750bd',
         center: { lat: 44.244629, lng: 4.197286 },
-        zoom: 14,
+        zoom: 9,
     });
 
-    new google.maps.Marker({
-        position: { lat: 44.244629, lng: 4.197286 },
+    const pinSettings = new PinElement({
+        borderColor: "#000000",
+        glyphColor: "#000000",
+        background: "#fed136",
+        scale: 1.15,
+    })
+
+    const MasDeBarras = new AdvancedMarkerElement({
+        position: { lat: 44.245287, lng: 4.196951 },
         map,
         title: "Mas de Barras",
+        content: pinSettings.element,
     });
 
-    const request = {
-        placeId: "ChIJzSTxgthbtBIRf84cyF11AHE",
-        fields: ["name", "formatted_address", "place_id", "geometry"],
-    };
     const infowindow = new google.maps.InfoWindow();
     const service = new google.maps.places.PlacesService(map);
-
-    service.getDetails(request, (place, status) => {
-    if (
-        status === google.maps.places.PlacesServiceStatus.OK &&
-        place &&
-        place.geometry &&
-        place.geometry.location
-        ) {
-            const marker = new google.maps.Marker({
-              map,
-              position: place.geometry.location,
-            });
-
-            google.maps.event.addListener(marker, "click", () => {
-                const content = document.createElement("div");
-                const nameElement = document.createElement("h2");
-
-                nameElement.textContent = place.name;
-                content.appendChild(nameElement);
-
-                const placeIdElement = document.createElement("p");
-
-                placeIdElement.textContent = place.place_id;
-                content.appendChild(placeIdElement);
-
-                const placeAddressElement = document.createElement("p");
-
-                placeAddressElement.textContent = place.formatted_address;
-                content.appendChild(placeAddressElement);
-                infowindow.setContent(content);
-                infowindow.open(map, marker);
-            });
-        }
-    });
 
     const input = document.getElementById("pac-input");
     const autocomplete = new google.maps.places.Autocomplete(input, {
@@ -99,13 +84,238 @@ async function initMap() {
             infowindowContent.children.namedItem("place-address").textContent = place.formatted_address;
             infowindow.open(map, marker);
         });
+}
+
+function addHistorical() {
+    clearOverlays();
+
+    map.setCenter({lat: 44.133675, lng: 4.520904});
+    map.setZoom(9.5);
+
+    const ChateauDeMontalet = {
+        placeId: "ChIJRfV96E5ZtBIRrd2P-sMjUuY",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const Montclus = {
+        placeId: "ChIJoUAp-hOrtRIRN5TuqkO5zvU",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const Barjac = {
+        placeId: "ChIJsy8fkIhVtBIR0EZrFiGIBwQ",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const Lussan = {
+        placeId: "ChIJYfDnBW9NtBIRUEN1K3-SSW0",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const LabastideDeVirac = {
+        placeId: "ChIJfWz59LMBtRIR8I7ry688CQQ",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const SainteMartinDArdeche = {
+        placeId: "ChIJjyOmEfintRIRgIXry688CQQ",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const Orange = {
+        placeId: "ChIJ4bzudbOQtRIR-oFFhNWKHa0",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const PontDuGard = {
+        placeId: "ChIJ1QTnSMLItRIR4uqG8xlzsSY",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const Nimes = {
+        placeId: "ChIJOVPo1gsttBIRAwwgn08TiN4",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const Avignon = {
+        placeId: "ChIJB528OYfrtRIRNnsd-m6bQuY",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const Uzes = {
+        placeId: "ChIJZ7QNZ0C1tRIR8DNrFiGIBwQ",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+
+    cha = addMarker(ChateauDeMontalet, "12th century castle, partly ruined with staterooms now restored.", "https://clnunez13.wordpress.com");
+    addMarker(Montclus, "Montclus is a tiny beautiful village and great place for walkers.", "https://www.francethisway.com/places/montclus.php");
+    addMarker(Barjac, "Lovely old village.", "https://www.francethisway.com/places/barjac.php");
+    addMarker(Lussan, "Village of character with 2 ruined castles.", "https://www.francethisway.com/places/lussan.php");
+    addMarker(LabastideDeVirac, "Chateau des Roure, medieval castle with living museum.", "www.chateaudesroure.com");
+    addMarker(SainteMartinDArdeche, "Like the Pont D’Arc in miniature.", "https://www.francethisway.com/places/a/saint-martin-d-ardeche-ardeche.php");
+    addMarker(Orange, "Spectacular Roman amphitheater – UNESCO World Heritage Site", "https://www.francethisway.com/places/orange.php");
+    addMarker(PontDuGard, "UNESCO World Heritage Site and perhaps one of the most famous roman aqueducts in the world.", "https://pontdugard.fr/en");
+    addMarker(Nimes, "Important outpost of the Roman empire, many well preserved Roman monuments including an Amphitheatre.", "https://www.francethisway.com/places/nimes.php");
+    addMarker(Avignon, "UNESCO World Heritage Site – Palais des Papes and the famous bridge and walled city.", "https://www.avignon-et-provence.com/en/tourism-provence/avignon");
+    addMarker(Uzes, "Commune in the Gard department in the Occitanie region", "https://www.france-voyage.com/tourism/uzes-1420.html");
+
+}
+
+function addActivities() {
+    clearOverlays();
+
+    map.setCenter({ lat: 44.244629, lng: 4.197286 });
+    map.setZoom(9.7);
+
+    const LesChevauxDuCantaure = {
+        placeId: "ChIJzSTxgthbtBIRf84cyF11AHE",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const CapCanoe = {
+        placeId: "ChIJC-g2UJJZtBIRCnaJka67SUs",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const AcrobrancheIndyParc = {
+        placeId: "ChIJZfw0KUX-tBIRw5oV25QAfMI",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const VallonPontDArc = {
+        placeId: "ChIJVZNCsPX9tBIRgrGltYohwLA",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const DinopediaParc = {
+        placeId: "ChIJKUIHCtpmtBIRZ56YHMVcrcI",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const Villefort = {
+        placeId: "ChIJRwPtIAmPtBIRlShSGGkqQQo",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const Prehistorama = {
+        placeId: "ChIJ75pARqxdtBIRNHJ_5hcCwrI",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const MuseumHarribo = {
+        placeId: "ChIJCTkOP5PKtRIRiohvwIvjiHQ",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+
+    addMarker(LesChevauxDuCantaure, "Horse riding.", "https://www.leschevauxducantaure.com");
+    addMarker(CapCanoe, "Canoe Hire", "https://capcanoe.fr");
+    addMarker(AcrobrancheIndyParc, "Treetop rope adventure park.", "https://www.indy-parc.com");
+    addMarker(VallonPontDArc, "Starting point for many companies offering canoeing adventures", "https://www.france-voyage.com/cities-towns/vallon-pont-d-arc-29653.htm");
+    addMarker(DinopediaParc, "Dinosaur Park", "www.dinopedia-parc.fr");
+    addMarker(Villefort, "Lake with Climbing, Caving and Watersports", "www.grandeurnature.48.com https://www.france-voyage.com/cities-towns/villefort-16898.htm");
+    addMarker(Prehistorama, "Prehistoric era museum.", "http://www.prehistorama.com");
+    addMarker(MuseumHarribo, "Museum about the Harribo sweets.", "l.musee@haribo.com");
+}
+
+function addLandscapes() {
+    clearOverlays();
+
+    map.setCenter({ lat: 44.294175, lng: 4.287948 });
+    map.setZoom(11);
+
+    const AroundSainteAmbroix = {
+        placeId: "ChIJl6dPpgpZtBIR1pExh5l7qW8",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const LesPetitesAiguieres = {
+        placeId: "ChIJT7QzJiNQtBIRSaJlJOamln0",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const PontDArc = {
+        placeId: "ChIJtSym8PIBtRIRTb7zGVPYPkw",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const GrotteDeLaCocaliere  = {
+        placeId: "ChIJc6PlFrxYtBIR6Zj4Vz0iWKI",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const GrotteDeLaSalamandre = {
+        placeId: "ChIJ-_v6CJlTtBIR-qTRHMESBhM",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+
+    addMarker(AroundSainteAmbroix, "Sainte Ambroix River Beach", "https://www.saint-ambroix.fr/decouvrir-bouger/situation-geographique/la-ceze");
+    addMarker(LesPetitesAiguieres, "Water-carved rock formations, with beautiful blue pools and waterfalls.", "https://www.theadventuredogs.com/gard/les-petites-aiguieres-avec-son-chien/");
+    addMarker(PontDArc, "A huge, natural rock arch on the Ardèche river.", "https://www.francethisway.com/places/pontdarc.php");
+    addMarker(GrotteDeLaCocaliere, "One of France's greatest caves", "https://www.grotte-cocaliere.com/en/");
+    addMarker(GrotteDeLaSalamandre, "A cave for the whole family decorated with the Crystal Giants.", "https://www.grottedelasalamandre.com/fr/");
+}
+
+function addShops() {
+    clearOverlays();
+
+    map.setCenter({ lat: 44.196733, lng: 4.147661 });
+    map.setZoom(11.5);
+
+    const Lidl = {
+        placeId: "ChIJQRhYmZ1ZtBIRXPaiy2UpLt8",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const Intermarche = {
+        placeId: "ChIJheTAfo9ZtBIRfFn1b57zs80",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const Market = {
+        placeId: "ChIJiS1PRY5ZtBIRaGobkD0FsIg",
+        fields: ["name", "formatted_address", "geometry"],
+    };
+    const Decathlon = {
+        placeId: "ChIJw1-FHDdCtBIRZoc5BL_iwzQ",
+        fields: ["name", "formatted_address", "geometry"],
+    };
 
 
-
-
-
-
+    addMarker(Lidl, "Supermarket.", "https://www.lidl.fr/");
+    addMarker(Intermarche, "Supermarket with gas station and laundromat.", "https://www.intermarche.com/magasin?pdvref=10644&utm_source=gmb");
+    addMarker(Market, "Discount supermarket.", "https://www.carrefour.fr/magasin/market-saint-ambroix");
+    addMarker(Decathlon, "Sports shop.", "https://www.decathlon.fr/store-view/magasin-de-sports-al%C3%A8s-0070049800498");
 }
 
 
 
+function addMarker(request, description, link) {
+
+    const infowindow = new google.maps.InfoWindow();
+    const service = new google.maps.places.PlacesService(map);
+
+    service.getDetails(request, (place, status) => {
+    if (
+        status === google.maps.places.PlacesServiceStatus.OK &&
+        place &&
+        place.geometry &&
+        place.geometry.location
+        ) {
+            const marker = new google.maps.Marker({
+              map,
+              position: place.geometry.location,
+            });
+
+            markersArray.push(marker);
+            google.maps.event.addListener(marker, "click", () => {
+                const content = document.createElement("div");
+                const nameElement = document.createElement("h2");
+
+                nameElement.textContent = place.name;
+                content.appendChild(nameElement);
+
+                const descriptionElement = document.createElement("p");
+
+                descriptionElement.textContent = description;
+                content.appendChild(descriptionElement);
+
+                const linkElement = document.createElement("a");
+
+                linkElement.textContent = link;
+                linkElement.href = link;
+                content.appendChild(linkElement);
+
+                const placeAddressElement = document.createElement("p");
+
+                placeAddressElement.textContent = place.formatted_address;
+                content.appendChild(placeAddressElement);
+                infowindow.setContent(content);
+                infowindow.open(map, marker);
+            });
+        }
+    });
+}
+
+function clearOverlays() {
+  for (var i = 0; i < markersArray.length; i++ ) {
+    markersArray[i].setMap(null);
+  }
+  markersArray.length = 0;
+}
--- website/views.py
+++ website/views.py
@@ -4,7 +4,7 @@ import smtplib
 import phonenumbers
 import json
 
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User, Review, Event
 from .initAdmin import getAdmin
 from . import initAdmin, db
@@ -34,7 +34,7 @@ def checkEmail(email):
         return False
 
 def checkPhone(number):
-    if carrier._is_mobile(number_type(phonenumbers.parse(number))):
+    if len(number) > 0:
         return True
     else:
         return False
@@ -51,8 +51,16 @@ def getEvents():
     return events
 
 @views.route('/', methods=['GET', 'POST'])
+def default():
+    return redirect(url_for('views.home'))
+
+@views.route('/en', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
+
+        if 'submit-lang' in request.form:
+             return redirect(url_for('views.homeFr'))
+
         if 'submit-login' in request.form:
             if request.form.get('username') == getAdmin().username:
                 if check_password_hash(getAdmin().password, request.form.get("password")):
@@ -62,7 +70,6 @@ def home():
                 flash("Incorrect username or password!", category='error')
 
         if 'submit-review' in request.form:
-
             user = request.form.get('review-name')
             description = request.form.get('review-description')
             rating = request.form.get('stars')
@@ -102,7 +109,6 @@ def home():
             db.session.commit()
 
         if 'submit-message' in request.form:
-
             name = request.form.get('message-name')
             email = request.form.get('message-email')
             arrival = request.form.get('message-arrival')
@@ -115,7 +121,7 @@ def home():
             elif len(name) < 1:
                 flash("Please use your real name", category='error')
             elif not checkPhone(phone):
-                flash("Please enter a valid phone number with country code, e.g. +44 1234 123456", category='error')
+                flash("Please enter a valid phone number, e.g. +44 1234 123456 or 01234 123456", category='error')
             elif len(message) < 10:
                 flash("Please enter a message longer than 10 characters", category='error')
             elif not checkDateFormat(arrival):
@@ -139,7 +145,100 @@ def home():
 
                 flash("Message sent!", category='success')
 
-    return render_template("/index.html", user=current_user, reviews=getReviews())
+    return render_template("/en.html", user=current_user, reviews=getReviews())
+
+@views.route('/fr', methods=['GET', 'POST'])
+def homeFr():
+    if request.method == 'POST':
+
+        if 'submit-lang' in request.form:
+            return redirect(url_for('views.home'))
+
+        if 'submit-login' in request.form:
+            if request.form.get('username') == getAdmin().username:
+                if check_password_hash(getAdmin().password, request.form.get("password")):
+                    flash("Login successful", category='success')
+                    login_user(getAdmin(), remember=True)
+            else:
+                flash("Incorrect username or password!", category='error')
+
+        if 'submit-review' in request.form:
+            user = request.form.get('review-name')
+            description = request.form.get('review-description')
+            rating = request.form.get('stars')
+            email = request.form.get('review-email')
+            title = request.form.get('review-title')
+
+            if rating == None:
+                flash("You must choose a rating!", category='error')
+            elif not checkEmail(email):
+                pass
+            elif len(description) < 10:
+                flash("Your review must be at least 10 characters long!", category='error')
+            elif len(title) < 1:
+                flash("Your title must be at least 1 characters long!", category='error')
+            elif len(user) < 1:
+                flash("Your name must be at least 1 characters long!", category='error')
+            else:
+                review = Review(user=user, description=description, rating=rating, email=email, title=title)
+                db.session.add(review)
+                db.session.commit()
+                flash("Review successfully submitted and under validation.", category='succes')
+
+        if 'submit-logout' in request.form:
+            logout_user()
+            flash("Logout successful", category='success')
+
+        if 'submit-review-approval' in request.form:
+            reviewID = request.form.get('submit-review-approval')
+            review = Review.query.filter_by(id=reviewID).first()
+            review.verified = True
+            db.session.commit()
+
+        if 'submit-review-delete' in request.form:
+            reviewID = request.form.get('submit-review-delete')
+            review = Review.query.filter_by(id=reviewID).first()
+            db.session.delete(review)
+            db.session.commit()
+
+        if 'submit-message' in request.form:
+            name = request.form.get('message-name')
+            email = request.form.get('message-email')
+            arrival = request.form.get('message-arrival')
+            departure = request.form.get('message-departure')
+            phone = request.form.get('message-phone')
+            message = request.form.get('message-message')
+
+            if not checkEmail(email):
+                pass
+            elif len(name) < 1:
+                flash("Please use your real name", category='error')
+            elif not checkPhone(phone):
+                flash("Please enter a valid phone number, e.g. +44 1234 123456 or 01234 123456", category='error')
+            elif len(message) < 10:
+                flash("Please enter a message longer than 10 characters", category='error')
+            elif not checkDateFormat(arrival):
+                flash("Please enter a valid arrival date", category='error')
+            elif not checkDateFormat(departure):
+                flash("Please enter a valid departure date", category='error')
+            else:
+                msg = MIMEMultipart()
+                msg['From'] = os.environ.get("email")
+                msg['To'] = os.environ.get("email")
+                msg['Subject'] = "New Message from " + email + " via Mas de Barras Website."
+                message = f"Email: {email}\nPhone Number: {phone}\n\nArrival Date: {arrival}\nDeparture Date: {departure}\n\nMessage: {message}\n\nFrom,\n{name}"
+                msg.attach(MIMEText(message))
+
+                server = smtplib.SMTP("smtp-mail.outlook.com", 587)
+                server.ehlo()
+                server.starttls()
+                server.ehlo()
+                server.login(os.environ.get("email"), os.environ.get("password"))
+                server.sendmail(os.environ.get("email"), os.environ.get("email"), msg.as_string())
+
+                flash("Message sent!", category='success')
+
+    return render_template("/fr.html", user=current_user, reviews=getReviews())
 
 @views.route('/calendar', methods=['POST'])
 def calendar():
