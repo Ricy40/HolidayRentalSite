4a40269c882aec8955b1f4931066edcd3cf71753
Ricy40 <ricardogarcia.pt@outlook.com>
2023-10-28 18:08:50 +0100

    New images and lang files

--- website/initAdmin.py
+++ website/initAdmin.py
@@ -1,3 +1,4 @@
+import os
 from .models import User
 from . import db
 from werkzeug.security import generate_password_hash
@@ -5,7 +6,7 @@ from werkzeug.security import generate_password_hash
 def getAdmin():
     admin = User.query.filter_by(id="1").first()
     if not admin:
-        admin = User(id="1", username="Honey", password=generate_password_hash("password"))
+        admin = User(id="1", username=os.environ.get("login_name"), password=generate_password_hash(os.environ.get("login_password")))
         db.session.add(admin)
         db.session.commit()
     return admin
